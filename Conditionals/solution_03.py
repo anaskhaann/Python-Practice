eb3d629d47e7447520090d5112f8150747bad163
Mohd Anas Khan <anasnazmulhasan@gmail.com>
2024-10-13 20:10:22 +0530

    Solution 3 Updated

--- Conditionals/solution_03.py
+++ Conditionals/solution_03.py
@@ -1,16 +1,28 @@
 # Assign a letter grade based on a student's score: A (90-100), B (80-89), C (70-79), D (60-69), F (below 60).
 
-# My Solution
+# Updated Solution
 score=int(input("Enter the Score:"))
 
-if score>=90:
-    print("The Student's Grade is A")
-elif (score>=80):
-    print("The Student's Grade is B")
-elif (score>=70):
-    print("The Student's Grade is C")
-elif (score>=60):
-    print("The Student's Grade is D")
-else:
-    print("The Student's Grade is F")
+if score>=101:
+    print("Enter the Correct Score Between 0-100")
+    exit()
+else:        
+    if score>=90:
+        # print("The Student's Grade is A")
+        grade="A"
+    elif (score>=80):
+        # print("The Student's Grade is B")
+        grade="B"
+    elif (score>=70):
+        # print("The Student's Grade is C")
+        grade="C"
+    elif (score>=60):
+        # print("The Student's Grade is D")
+        grade="D"
+    else:
+        # print("The Student's Grade is F")
+        grade="F"
+
+print(f"Grade of the Student is {grade}")
+
     
