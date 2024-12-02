64dafcec6e2d389965b4d57db9e6948daa08e4c3
Mohd Anas Khan <119879801+anasskhannn@users.noreply.github.com>
2024-12-02 18:42:52 +0530

    Add solution 8

---
+++ Loops/solutions/solution_8.py
@@ -0,0 +1,13 @@
+# Problem: Check if a number is prime.
+
+num=int(input("Enter the number to check: "))
+
+if num>1:
+    for i in range(2,num):
+        if (num%i)==0:
+            print(f"{num} is not a prime number")
+            break
+        else:
+            print(f"{num} is a prime number")
+            break
+            
