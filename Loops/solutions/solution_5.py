4bc353c0fc195b792231830a00b8180e6b7e54e5
Mohd Anas Khan <119879801+anasskhannn@users.noreply.github.com>
2024-12-02 17:55:29 +0530

    Modify solution 5 According to problem statement

--- Loops/solutions/solution_5.py
+++ Loops/solutions/solution_5.py
@@ -5,4 +5,5 @@ for char in input_str:
     print(char)
     # Inbuid count method to check occurance of each characters
     if input_str.count(char)==1:
-        print(f"Non Repeated Character is:{char}")
+        print(f"First Non Repeated Character is:{char}")
+        break
