a90f977cf638103e2ec0b99c9dec04452895bb24
Mohd Anas Khan <119879801+anasskhannn@users.noreply.github.com>
2024-12-05 20:45:18 +0530

    Add Example code of Closure

--- Scopes&Closure/main.py
+++ Scopes&Closure/main.py
@@ -1,11 +1,17 @@
-x=10
-
-def f1():
-    x=20
-    def f2():
-        print(x)
+# Define a function f1 that takes a number num and returns another function
+def f1(num):
+    def f2(y):
+        return y ** num 
     return f2
 
-result= f1()
-print(result)
-result()
+# Create two instances of f2 with different values of num
+a = f1(2)  # a will raise its input to the power of 2
+b = f1(3)  # b will raise its input to the power of 3
+
+# Print the memory addresses of the f2 functions
+print(a)
+print(b)
+
+# Call the f2 functions with y=5
+print(a(5))  # Should print 25 (5^2)
+print(b(5))  # Should print 125 (5^3)
