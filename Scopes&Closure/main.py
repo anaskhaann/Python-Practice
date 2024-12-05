b1b13cf7671816342264db9a7365d7f572f29cdb
Mohd Anas Khan <119879801+anasskhannn@users.noreply.github.com>
2024-12-05 20:01:21 +0530

    Add Sample code on Global var changing

--- Scopes&Closure/main.py
+++ Scopes&Closure/main.py
@@ -1,8 +1,16 @@
 x = 99
 
-def calc(y):
-    # Here x is global and y is local
-    z=x+y
-    return z
+# def calc(y):
+#     # Here x is global and y is local
+#     z=x+y
+#     return z
 
-print(calc(3))
+# print(calc(3))
+
+
+def func2():
+    # This will not change the value of x globally
+    x= 10
+    
+func2()
+print(x)
