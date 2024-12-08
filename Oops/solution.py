e37a1daa13714f740ca690b0225adb6dd995d812
Mohd Anas Khan <119879801+anasskhannn@users.noreply.github.com>
2024-12-08 12:45:01 +0530

    3rd Commit

--- Oops/solution.py
+++ Oops/solution.py
@@ -1,15 +1,14 @@
-# Problem: Create a Car class with attributes like brand and model. Then create an instance of this class.
-
-
 class Car():
-    # self is used to establish a connection to access the class attributes(variables)
     def __init__(self,brand,model):
-        
-        # Self.brand means class one and RHS brand means which user is giving as attribute while creating object
         self.brand = brand
         self.model = model
 
-my_car = Car("BMW", "M5")
+    def full_name(self):
+        return f"{self.brand} {self.model}"
+
+my_car = Car("Toyota", "Supra")
 print(my_car) 
 print(my_car.brand) 
 print(my_car.model) 
+# paranthesis because it is a functionality
+print(my_car.full_name()) 
