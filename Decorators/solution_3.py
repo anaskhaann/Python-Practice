2390673a6a87a451a9fc12a0932a455e018efbc9
Mohd Anas Khan <119879801+anasskhannn@users.noreply.github.com>
2024-12-09 14:25:04 +0530

    Add boiler plate of solution 3

--- Decorators/solution_3.py
+++ Decorators/solution_3.py
@@ -1,1 +1,14 @@
 # Problem: Implement a decorator that caches the return values of a function, so that when it's called with the same arguments, the cached value is returned instead of re-executing the function.
+
+import time
+
+
+def cache(func):
+    def wrapper(*args):
+        pass
+    return wrapper
+
+
+
+def long_running_function(a,b):
+    return a**b
