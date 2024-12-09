77a1cced2d1f2e38c2660860c7f07922633871bd
Mohd Anas Khan <119879801+anasskhannn@users.noreply.github.com>
2024-12-09 12:43:13 +0530

    Add solution 2 with explaination

--- Decorators/solution_2.py
+++ Decorators/solution_2.py
@@ -1,15 +1,18 @@
 # Problem: Create a decorator to print the function name and the values of its arguments every time the function is called.
 
 def debug(func):
-    
-    def wrapper(*args,**kwargs):
-    
-        args_value=", ".join(str(arg) for arg in args)
-        kwrgs_value=", ".join(f"{k}:{v}" for k,v in kwargs.items())
-    
-        print( f" Calling the Function: {func.__name__},With Arguments: {args_value} And KeyWord Arguments: {kwrgs_value} " )
-    
-        return func(*args,**kwargs)
+    def wrapper(*args, **kwargs):
+        # Format the positional arguments into a string
+        args_value = ", ".join(str(arg) for arg in args)
+        
+        # Format the keyword arguments into a string
+        kwrgs_value = ", ".join(f"{k}:{v}" for k, v in kwargs.items())
+        
+        # Print a message indicating the function name and its arguments
+        print(f"Calling the Function: {func.__name__}, With Arguments: {args_value} And Keyword Arguments: {kwrgs_value}")
+        
+        # Call the original function 'func' with the original arguments and return its result
+        return func(*args, **kwargs)
     
     return wrapper
 
