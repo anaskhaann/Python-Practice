ccece0f740be324ebda10272625be6398deecc54
Mohd Anas Khan <119879801+anasskhannn@users.noreply.github.com>
2024-12-18 19:26:42 +0530

    Add syntax 2 for error handling in file

--- Error Handling/error_handling.py
+++ Error Handling/error_handling.py
@@ -1,11 +1,19 @@
 # Two Syntax for File handling
 
-file = open("youtube.txt","r") #w means write mode it will create file if not present
+# # Syntax 1
+# file = open("youtube.txt","w") #w means write mode it will create file if not present
 
-# to write something we have to be cautious since maybe it is a imp ifle
+# # to write something we have to be cautious since maybe it is a imp ifle
 
-try:
-    file.write("Hello World")
-finally:
-    # Close the file once done
+# try:
+#     file.write("Hello World")
+# finally:
+#     # Close the file once done
+#     file.close()
+
+
+# Syntax 2
+
+with open("youtube.txt", "w") as file:
+    file.write("This is written using syntax 2")
     file.close()
