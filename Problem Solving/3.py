081a81608a84dba3019c818abc7258e6d17ccd1c
Mohd Anas Khan <119879801+anasskhannn@users.noreply.github.com>
2025-05-01 16:27:32 +0530

    Added Problem 3

---
+++ Problem Solving/3.py
@@ -0,0 +1,22 @@
+"""
+Problem Statement: Check for Palindrome
+Write a Python function that checks whether a given string is a palindrome or not.
+A string is said to be a palindrome if it reads the same backward as forward, ignoring case and non-alphanumeric characters.
+
+Example Input/Output:
+✅ Input: "Madam"
+🔄 Output: True
+
+✅ Input: "A man, a plan, a canal, Panama"
+🔄 Output: True
+
+✅ Input: "hello"
+🔄 Output: False
+
+Constraints:
+Ignore case sensitivity.
+
+Ignore spaces, punctuation, and special characters.
+
+You cannot use [::-1] or reversed().
+"""
